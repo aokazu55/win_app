8ec2f3e4cef4361551c7a37f7d8fddf0a8d797a1
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-07-05 09:37:48 +0900

    Edit:users_controller,routes

--- .history/app/models/user_20200704225621.rb
+++ .history/app/models/user_20200704225621.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, :
-  attachment :profile_image
+         :recoverable, :rememberable, :validatable
 end
