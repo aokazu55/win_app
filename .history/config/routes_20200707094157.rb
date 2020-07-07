a08a92d0d488b60e06079d2d782bb2c6e59ec8ef
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-07-07 11:33:00 +0900

    Add:devise認証メール設定と管理者権限設定のためcancancaとrails_adminのインストールと設定

---
+++ .history/config/routes_20200707094157.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'users#index'
+  devise_for :users
+  resources :users
+  resources :services
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
