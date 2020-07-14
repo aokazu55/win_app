5d64778dfd00ab9c09794c3fd21ecd5726861b9e
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-07-14 20:08:34 +0900

    Fix:コメント部分の調整のためuserテーブルとcommentテーブルもアソシエーションを組む

---
+++ .history/app/controllers/comments_controller_20200714200522.rb
@@ -0,0 +1,57 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_service, only: [:create, :edit, :update, :destroy]
+
+  def create
+    @comment = @service.current_user.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        flash.now[:notice] = 'コメントが投稿されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの投稿に失敗しました'
+        format.js { render :error }
+      end
+    end
+  end
+
+  def edit
+    @comment = @service.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @service.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+      @comment.destroy
+      respond_to do |format|
+        flash.now[:notice] = 'コメントが削除されました'
+        format.js { render :index }
+      end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :service_id)
+  end
+
+  def set_service
+    @service = Service.find(params[:service_id])
+  end
+end
---
+++ .history/app/controllers/services_controller_20200714102953.rb
@@ -0,0 +1,56 @@
+class ServicesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+
+    def show
+      @comments = @service.comments
+      @comment = @service.comments.build
+    end
+
+    def index
+      @services = Service.all.order(created_at: :desc)
+    end
+
+    def new
+      @service = Service.new
+    end
+
+    def create
+      @service = current_user.services.build(service_params)
+      if @service.save
+        redirect_to service_path(@service), notice: "サービスを投稿しました"
+      else
+        render :new
+      end
+    end
+
+    def edit
+      if @service.user != current_user
+        redirect_to services_path, alert: "不正なアクセスです"
+      end
+    end
+
+    def update
+      if @service.update(service_params)
+        redirect_to service_path(@service), notice: "サービスを更新しました"
+      else
+        render :edit, alert: "更新できませんでした"
+      end
+    end
+
+    def destroy
+      service = Service.find(params[:id])
+      service.destroy
+      redirect_to user_path(service.user), notice: "サービスを削除しました。"
+    end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:service_name, :main_service, :service_detail, :price, :remark, :image, :working_time, :confidence_level, :period_start, :period_end, :transportation_expenses)
+  end
+end
