d5e93f8ac9b9748807410124ebe766010dba94ab
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-07-20 12:49:57 +0900

    画像複数投稿の実装に入る

---
+++ .history/app/models/service_20200717114907.rb
@@ -0,0 +1,8 @@
+class Service < ApplicationRecord
+  attachment :image
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User"
+end
